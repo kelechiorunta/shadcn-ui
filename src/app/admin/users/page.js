57b9f6ec0aec41fdd6fc56d20b27ff86dc060166
Kelechi Orunta <kelechiorunta1@gmail.com>
2024-10-18 00:06:12 +0100

    Initial setup on the shadcn-ui

--- src/app/admin/users/page.js
+++ src/app/admin/users/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>
+    <div className='min-h-screen flex flex-col px-8'>
       <ServiceProviderApplications/>
     </div>
   )
